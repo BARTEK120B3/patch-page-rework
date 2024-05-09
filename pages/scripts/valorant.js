fb142e6f1645c2108fda8e8bdb6fb0d0f2ba030f
Bartosz Bartocha <bbbartekbartocha@gmail.com>
2024-05-09 17:39:36 +0200

    YESSSS finnaly the mobile animation works

--- pages/scripts/valorant.js
+++ pages/scripts/valorant.js
@@ -1,17 +1,14 @@
 const popup = document.getElementsByClassName("popup-menu");
 const szczala = document.getElementsByClassName("szczala");
-const championImg = document.getElementsByClassName("champion-img");
+const championImgs = document.getElementsByClassName("champion-img");
 
 const animations = ["rotateX", "rotateY", "rotateZ"];
-const imgSrc = [
-  "src/brim.jpg",
-  "src/harbor.jpg",
-  "src/iso.jpg",
-  "src/viper.jpg",
-];
 
-let previousRandom = null;
 let random = null;
+let previousRandom = null;
+
+let imgRand = null;
+
 function giveRandom(x) {
   random = getRandomInt(x);
   while (random === previousRandom) {
@@ -46,19 +43,6 @@ function slideOut() {
   });
 }
 
-function changeChamp() {
-  return new Promise((resolve) => {
-    championImg[0].style.animationName = "hideChamp";
-    setInterval(() => {
-      championImg[0].src = imgSrc[giveRandom(4)];
-      setInterval(()=>{
-        championImg[0].style.animationName = "showChamp";
-      },500)
-      resolve();
-    }, 3000);
-  });
-}
-
 async function triggerAnimation() {
   if (popup[0].style.display == "block") {
     await slideOut();
@@ -67,6 +51,57 @@ async function triggerAnimation() {
   }
 }
 
-// setInterval(changeChamp()[0], 3500);
+/*
+
+mobile version img changing algorithm - function champChamge()
+
+loop : 
+
+get random (use giveRandom so the numebr won't be same as previous) img and show it on screen with animation => {
+  get random number between 0 and 3
+  display block the img based on the number
+} - animation should take smth about 2 seconds
+
+wait 3 seconds => {
+  time sleep for 3 seconds
+}
+
+take off the image with animation => {
+  give aniamtion name so the img can fade out
+  give the img display none
+} - animation should take smth about 2 seconds
+
+rename the animation for the next iteration => {
+  reset the animation name for the img
+}
+
+---------------------------------
+
+needed variables:
+
+the variable that holds the random number 
+
+the list of imgs
+
+*/
 
 changeChamp();
+
+function changeChamp() {
+  return new Promise((resolve) => {
+    imgRand = giveRandom(4); // this will give us the number from 0 to 3
+    console.log(imgRand);
+    championImgs[imgRand].style.display = "block";
+    setTimeout(() => {
+      championImgs[imgRand].style.animationName = "hideChamp";
+      setTimeout(() => {
+        championImgs[imgRand].style.display = "none";
+        championImgs[imgRand].style.animationName = "showChamp";
+        console.log(imgRand, " has ", championImgs[imgRand].style.display);
+        resolve();
+      }, 2000);
+    }, 6000);
+  });
+}
+
+setInterval(changeChamp, 8050);
